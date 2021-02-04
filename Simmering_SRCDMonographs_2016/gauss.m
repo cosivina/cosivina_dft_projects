fdd26320d938c199ac04d0b24bcb243064ac713c
John Spencer <j.spencer@uea.ac.uk>
2021-02-04 10:27:56 +0000

    deleting in progress

--- Simmering_SRCDMonographs_2016/gauss.m
+++
@@ -1,6 +0,0 @@
-function y = gauss(n,n0,G0,sigma,k)
-
-x= 0:n;
-
-y = G0 * exp(-0.5 * (x-n0).^2/sigma.^2) + k;
-
--- Simmering_SRCDMonographs_2016/sigmoid.m
+++
@@ -1,3 +0,0 @@
- function s = sigmoid(x,beta,x0)
-
- s = 1./ (1 + exp(-beta*(x-x0)));
